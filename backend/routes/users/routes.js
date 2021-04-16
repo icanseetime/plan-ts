4abf547889c32555df95ea6df04cd523b2c1bc63
Ida <imgjeits@stud.ntnu.no>
2021-04-16 22:23:12 +0200

    cleanup, remove exam-specific routes

--- backend/routes/users/routes.js
+++ backend/routes/users/routes.js
@@ -44,7 +44,7 @@ const inviteUser = async (req, res) => {
     }
 }
 
-// GET: Check for invite and retrieve information //TODO: test
+// GET: Check for invite and retrieve information
 const checkInvite = async (req, res) => {
     try {
         let existingInvite = await Invite.findOne({ _id: req.params.id })
@@ -155,7 +155,7 @@ const listUsers = async (req, res) => {
     }
 }
 
-// GET: Get info about specific user by their ID (manager)
+// GET: Get info about specific user by their ID
 const getUser = async (req, res) => {
     try {
         // Find user (but do not return password and version no.)
@@ -172,37 +172,7 @@ const getUser = async (req, res) => {
     }
 }
 
-// PUT: Update user details (except password & ID!)
-const updateUserDetails = async (req, res) => {
-    try {
-        // Make sure request doesn't include password
-        if (!req.body.password) {
-            // Update user and get response
-            const user = await User.findByIdAndUpdate(req.params.id, req.body) // TODO: update name only works with "name.first" and "name.last" - check if there is a better solution
-            console.log(user)
-            // Check for found user
-            if (!user) {
-                res.status(404).json({
-                    error: `Could not find a user with the ID ${req.params.id}.`
-                })
-            } else {
-                res.status(201).json({
-                    message: `User with ID ${req.params.id} was successfully updated.`
-                })
-            }
-        } else {
-            res.status(403).json({
-                error: `You should not include user passwords in your request.` // TODO: include more info in this error message when "password-reset" functionality is ready
-            })
-        }
-    } catch (err) {
-        res.status(500).json({
-            error: `Something went wrong while trying to update the user. [${err}]`
-        })
-    }
-}
-
-// DELETE: Delete user by ID (manager)
+// DELETE: Delete user by ID
 const deleteUser = async (req, res) => {
     try {
         // Check that user exists in DB
@@ -224,62 +194,6 @@ const deleteUser = async (req, res) => {
     }
 }
 
-// --------------------------- EXAM -----------------------------
-
-// // POST: Create new user (manager)
-// const createUser = async (req, res) => {
-//     try {
-//         // Check that user e-mail does not exist in DB
-//         let existingUser = await User.findOne({ email: req.body.email })
-//         if (existingUser) {
-//             res.status(409).json({
-//                 error: `A user with this email already exists in the database.` // TODO: check security
-//             })
-//         }
-
-//         // Create user object
-//         const newUser = new User({
-//             email: req.body.email,
-//             password: req.body.password,
-//             name: {
-//                 first: req.body.firstName,
-//                 last: req.body.lastName
-//             },
-//             role: req.body.role
-//         })
-
-//         // Save to DB and send response
-//         const user = await newUser.save()
-//         res.status(201).json({
-//             message: 'New user successfully created.',
-//             user: user
-//         })
-//     } catch (err) {
-//         res.status(500).json({
-//             error: `There was an error adding ${req.body.email} to the database. [${err}]`
-//         })
-//     }
-// }
-
-// POST: Reset user password
-const resetUserPass = async (req, res) => {
-    try {
-        let existingUser = User.findById(req.params.id)
-        if (!existingUser) {
-            res.status(404).json({ error: 'User not found.' })
-        }
-
-        // TODO:
-        // - Save "password-reset" in a new database with own ID + ID of the user that is having their password reset
-        // - Create URL using the new ID as the end of the URI (get site-url som env?)
-        // - Send URL in e-mail to the user with the given ID
-    } catch (err) {
-        res.status(500).json({
-            error: `Something went wrong while trying to reset password of user with ID ${req.params.id}. [${err}]`
-        })
-    }
-}
-
 module.exports = {
     inviteUser,
     checkInvite,
@@ -288,8 +202,6 @@ module.exports = {
     loginUser,
     listUsers,
     getUser,
-    // createUser,
-    // resetUserPass,
     updateUserDetails,
     deleteUser
 }
