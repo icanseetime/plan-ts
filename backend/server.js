9b3fc0c450da81962c452fb1022e0cf8380980be
Ida <imgjeits@stud.ntnu.no>
2021-05-18 22:45:30 +0200

    added port

--- backend/server.js
+++ backend/server.js
@@ -59,5 +59,5 @@ app.use((err, req, res, next) => {
 
 // Server
 app.listen(process.env.PORT || 5000, () =>
-    console.log(`✅ Server running [👂:${process.env.PORT}]`)
+    console.log(`✅ Server running [👂:${process.env.PORT || 5000}]`)
 )
